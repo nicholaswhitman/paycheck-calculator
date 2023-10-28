eeee8e317778d0717faebb8e63e07b276031d926
Nicholas Whitman <nicks.reverie@gmail.com>
2023-10-27 22:22:42 -0400

    added definition and commas to output

--- paycheck_calculator.py
+++ paycheck_calculator.py
@@ -2,164 +2,138 @@
 # Nicholas W
 # 20 May 2023
 
-medicare = .0145 					# 1.45%
-socialSecurity = .062 				# 6.2%
-FICA = medicare + socialSecurity	# 7.65%
-taxBracket1 = .10 					# 10%
-taxBracket2 = .12 					# 12%
-taxBracket3 = .22                   # 22%
-taxBracket4 = .24                   # 24%
-taxBracket5 = .32                   # 32%
-taxBracket6 = .35                   # 35%
-taxBracket7 = .37                   # 37% 
 
-grossWeekly = grossAnnual = 0.0
-cont = True
+def main():
 
-while cont == True:
+	medicare = .0145 					# 1.45%
+	socialSecurity = .062 				# 6.2%
+	FICA = medicare + socialSecurity	# 7.65%
+	taxBracket1 = .10 					# 10%
+	taxBracket2 = .12 					# 12%
+	taxBracket3 = .22                   # 22%
+	taxBracket4 = .24                   # 24%
+	taxBracket5 = .32                   # 32%
+	taxBracket6 = .35                   # 35%
+	taxBracket7 = .37                   # 37% 
 
-	userJobs = int(input("How many jobs do you have? "))
+	quit = False
+
+	while quit == False:
+
+		grossWeekly = grossAnnual = 0.0
+		userJobs = int(input("How many jobs do you have? "))
 	
-	for i in range(0, userJobs, 1):
-		grossHourly = float(input("What is your hourly pay? "))
-		hoursPerWeek = float(input("How many hours a week do you work? "))
+		for i in range(0, userJobs, 1):
+			grossHourly = float(input("What is your hourly pay? "))
+			hoursPerWeek = float(input("How many hours a week do you work? "))
 
-		tempGrossWeekly = grossHourly * hoursPerWeek
-		tempGrossAnnual = grossHourly * hoursPerWeek * 52
+			tempGrossWeekly = grossHourly * hoursPerWeek
+			tempGrossAnnual = grossHourly * hoursPerWeek * 52
 
-		grossWeekly += tempGrossWeekly
-		grossAnnual += tempGrossAnnual
+			grossWeekly += tempGrossWeekly
+			grossAnnual += tempGrossAnnual
 
-		print("Weekly gross income for job",i+1,":", tempGrossWeekly)
-		print("Annual gross income for job",i+1,":", tempGrossAnnual)
-		print("-----------------------------------------")
+			print("Weekly gross income for job",i+1, f": {tempGrossWeekly:,}")
+			print("Annual gross income for job",i+1, f": {tempGrossAnnual:,}")
+			print("-----------------------------------------")
 
-	if userJobs > 1:
-		print("Total weekly gross income: ", grossWeekly)
-		print("Total annual gross income: ", grossAnnual)
+		if userJobs > 1:
+			print(f"Total weekly gross income: {grossWeekly:,}")
+			print(f"Total annual gross income: {grossAnnual:,}")
 
-	if grossAnnual <= 11000:
-		temp = grossAnnual * (FICA + taxBracket1)
-		netAnnual = grossAnnual - temp
-		netWeekly = netAnnual/52
+		if grossAnnual <= 11000:
+			temp = grossAnnual * (FICA + taxBracket1)
+			netAnnual = grossAnnual - temp
+			netWeekly = netAnnual/52
 
-		if userJobs < 1: 
-			print("Weekly net income: ", netWeekly)
-			print("Annual net income: ", netAnnual)
+			output(userJobs, netWeekly, netAnnual)
 			print("Tax bracket 1")
-		else:
-			print("Total weekly net income: ", netWeekly)
-			print("Total annual net income: ", netAnnual)
-			print("Tax bracket 1")
-		pass
 
-	elif 11001 <= grossAnnual <=  44725:
-		temp = grossAnnual * FICA
-		temp2 = (grossAnnual - 11000) * taxBracket2
-		netAnnual = grossAnnual - (temp + temp2 + 1100)
-		netWeekly = netAnnual/52
+		elif 11001 <= grossAnnual <=  44725:
+			temp = grossAnnual * FICA
+			temp2 = (grossAnnual - 11000) * taxBracket2
+			netAnnual = grossAnnual - (temp + temp2 + 1100)
+			netWeekly = netAnnual/52
 
-		if userJobs < 1:
-			print("Weekly net income: ", netWeekly)
-			print("Annual net income: ", netAnnual)
-			print("Tax bracket 2")
-		else:
-			print("Total weekly net income: ", netWeekly)
-			print("Total annual net income: ", netAnnual)
+			output(userJobs, netWeekly, netAnnual)
 			print("Tax bracket 2")	
-		pass
 
-	elif 44756 <= grossAnnual <=  95375:
-		temp = grossAnnual * FICA
-		temp2 = (grossAnnual - 44725) * taxBracket3
-		netAnnual = grossAnnual - (temp + temp2 + 5127)
-		netWeekly = netAnnual/52
+		elif 44756 <= grossAnnual <=  95375:
+			temp = grossAnnual * FICA
+			temp2 = (grossAnnual - 44725) * taxBracket3
+			netAnnual = grossAnnual - (temp + temp2 + 5127)
+			netWeekly = netAnnual/52
 
-		if userJobs < 1:
-			print("Weekly net income: ", netWeekly)
-			print("Annual net income: ", netAnnual)
-			print("Tax bracket 3")
-		else: 
-			print("Total weekly net income: ", netWeekly)
-			print("Total annual net income: ", netAnnual)
+			output(userJobs, netWeekly, netAnnual)
 			print("Tax bracket 3")	
-		pass
 
-	elif 95376 <= grossAnnual <=  182100:
-		temp = grossAnnual * FICA
-		temp2 = (grossAnnual - 95375) * taxBracket4
-		netAnnual = grossAnnual - (temp + temp2 + 16290)
-		netWeekly = netAnnual/52
+		elif 95376 <= grossAnnual <=  182100:
+			temp = grossAnnual * FICA
+			temp2 = (grossAnnual - 95375) * taxBracket4
+			netAnnual = grossAnnual - (temp + temp2 + 16290)
+			netWeekly = netAnnual/52
 
-		if userJobs < 1:
-			print("Weekly net income: ", netWeekly)
-			print("Annual net income: ", netAnnual)
+			output(userJobs, netWeekly, netAnnual)
 			print("Tax bracket 4")
-		else: 
-			print("Total weekly net income: ", netWeekly)
-			print("Total annual net income: ", netAnnual)
-			print("Tax bracket 4")
-		pass
 
-	elif 182101 <= grossAnnual <=  231250:
-		temp = grossAnnual * FICA
-		temp2 = (grossAnnual - 182100) * taxBracket5
-		netAnnual = grossAnnual - (temp + temp2 + 37104)
-		netWeekly = netAnnual/52
+		elif 182101 <= grossAnnual <=  231250:
+			temp = grossAnnual * FICA
+			temp2 = (grossAnnual - 182100) * taxBracket5
+			netAnnual = grossAnnual - (temp + temp2 + 37104)
+			netWeekly = netAnnual/52
 
-		if userJobs < 1:
-			print("Weekly net income: ", netWeekly)
-			print("Annual net income: ", netAnnual)
+			output(userJobs, netWeekly, netAnnual)
 			print("Tax bracket 5")
+
+		elif 231251 <= grossAnnual <=  578125:
+			temp = grossAnnual * FICA
+			temp2 = (grossAnnual - 231250) * taxBracket6
+			netAnnual = grossAnnual - (temp + temp2 + 52832)
+			netWeekly = netAnnual/52
+
+			output(userJobs, netWeekly, netAnnual)
+			print("Tax bracket 6")
+
+		elif 578125 <= grossAnnual:
+			temp = grossAnnual * FICA
+			temp2 = grossAnnual * taxBracket7
+			netAnnual = (grossAnnual - 578125) - (temp + temp2 + 174238.25)
+			netWeekly = netAnnual/52
+
+			output(userJobs, netWeekly, netAnnual)
+			print("Tax bracket 7")
+
 		else: 
-			print("Total weekly net income: ", netWeekly)
-			print("Total Annual net income: ", netAnnual)
-			print("Tax bracket 5")
-		pass
+			print("Error ")
 
-	elif 231251 <= grossAnnual <=  578125:
-		temp = grossAnnual * FICA
-		temp2 = (grossAnnual - 231250) * taxBracket6
-		netAnnual = grossAnnual - (temp + temp2 + 52832)
-		netWeekly = netAnnual/52
+		print("Does not incorporate state and city taxes. ")
 
-		if userJobs < 1:
-			print("Weekly net income: ", netWeekly)
-			print("Annual net income: ", netAnnual)
-			print("Tax bracket 6")
-		else:
-			print("Total weekly net income: ", netWeekly)
-			print("Total annual net income: ", netAnnual)
-			print("Tax bracket 6")
-		pass
+		# end condition for while loop
+		userInput = input("Do you want to quit? ").strip().lower()
+		if userInput == 'no' or userInput == 'n':
+			print("-----------------------------------------")
+			quit = False
+		else: 
+			print("Now exiting.")
+			quit = True	
 
-	elif 578125 <= grossAnnual:
-		temp = grossAnnual * FICA
-		temp2 = grossAnnual * taxBracket7
-		netAnnual = (grossAnnual - 578125) - (temp + temp2 + 174238.25)
-		netWeekly = netAnnual/52
 
-		if userJobs < 1:
-			print("Weekly net income: ", netWeekly)
-			print("Annual net income: ", netAnnual)
-			print("Tax bracket 7")
-		else:
-			print("Total weekly net income: ", netWeekly)
-			print("Total annual net income: ", netAnnual)
-			print("Tax bracket 7")
-		pass
 
+
+
+def output(userJobs, netWeekly, netAnnual):
+	if userJobs == 1:
+		print(f"Weekly net income: {netWeekly:,}")
+		print(f"Annual net income: {netAnnual:,}")
 	else: 
-		print("Error ")
-
-	print("Does not incorporate state and city taxes. ")
-
-# end condition for while loop
-	userInput = input("Would you like to continue? ").strip().lower()
-	if userInput == 'yes' or userInput == 'y' or userInput == '1':
-		cont = True
-	else: 
-		print("Now exiting.")
-		cont = False	
+		print(f"Total Weekly net income:  {netWeekly:,}")
+		print(f"Total Annual net income:  {netAnnual:,}")
 
 
+
+
+
+
+
+
+main()
